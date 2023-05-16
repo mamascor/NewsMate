7b66f894deed13b7cf3bffef422b50a1b4705ed4
Marco Mascorro <marco.mascorroa@gmail.com>
2023-05-16 11:06:09 -0500

    Initial Commit

---
+++ NewsMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsMate
+//
+//  Created by Marco Mascorro on 5/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
